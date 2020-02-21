09fa61b473513fb55317dfe0cfd962695b352148
Victor Markov <victor.markov@urbanise.com>
2020-02-21 16:30:19 +0200

    Add context menu to list items

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
-import { ScrollView, Text, RefreshControl, View} from 'react-native';
+import { ScrollView, Text, RefreshControl, View, StyleSheet, Dimensions} from 'react-native';
 import { SearchBar, ListItem, Divider } from 'react-native-elements';
 import {SafeAreaView} from 'react-navigation'
 import BaseScreen from './BaseScreen';
@@ -11,6 +11,8 @@ import styles from '../styles/base';
 import { connect } from 'react-redux';
 import { loadProducts } from '../actions/products';
 import { bindActionCreators } from 'redux';
+import { SwipeListView } from 'react-native-swipe-list-view';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 function filterProducts(i, search) {
   return i.name.toLowerCase().indexOf(search.toLowerCase()) > -1;
@@ -77,13 +79,7 @@ class HomeScreen extends BaseScreen {
   getShoppingCartItems = () => {
     return this.state.shoppingListItems
       .filter(i => filterProducts(i, this.state.search))
-      .sort((a: any, b: any) => a.id - b.id)
-      .map((p: any, i) => (<ListItem
-        key={i}
-        title={p.name}
-        bottomDivider
-        onPress={() => this.removeFromShoppingList(p)}
-      />))
+      .sort((a: any, b: any) => a.id - b.id);
   }
 
   addToShoppingList = (item) => {
@@ -100,9 +96,23 @@ class HomeScreen extends BaseScreen {
       .then(this.loadShoppingCartItems);
   }
 
+  closeRow(rowMap, rowKey) {
+    if (rowMap[rowKey]) {
+        rowMap[rowKey].closeRow();
+    }
+  }
+
+  removeItemFromCart(rowMap, item) {
+    this.closeRow(rowMap, item._id);
+    this.removeFromShoppingList(item);
+  }
+
+
   render() {
     const { search, loading} = this.state;
-    const { products: {items: productsMeta} } = this.props;
+    const { products: {items: productsMeta}} = this.props;
+    const screenWidth = Math.round(Dimensions.get('window').width);
+    const swipeWidth = -screenWidth * 0.4;
     return (
       <SafeAreaView style={styles.safeAreaView}>
         <View style={styles.container}>
@@ -117,7 +127,36 @@ class HomeScreen extends BaseScreen {
           <RefreshControl refreshing={loading} onRefresh={this.loadShoppingCartItems} />
         }>
           <Divider></Divider>
-          {this.getShoppingCartItems()}
+          <SwipeListView
+            data={this.getShoppingCartItems()}
+            renderItem={ (data: any, rowMap:any) => {
+              const {item} = data;
+              return (
+                <ListItem
+                  key={item._id}
+                  title={item.name}
+                  bottomDivider
+              />
+            )}}
+            renderHiddenItem={ (data, rowMap) => (
+                <View style={stylesa.rowBack}>
+                    <View style={stylesa.rowPlaceholder}></View>
+                    <View style={stylesa.rowMenu}>
+                      <TouchableOpacity onPress={() => this.removeItemFromCart(rowMap, data.item)}>
+                        <Text>
+                          Right
+                        </Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity onPress={() => this.removeItemFromCart(rowMap, data.item)}>
+                        <Text>
+                          Right-Left
+                        </Text>
+                      </TouchableOpacity>
+                    </View>
+                </View>
+            )}
+            rightOpenValue={swipeWidth}
+        />
           <Divider></Divider>
           <Text>Products</Text>
           {this.getProducts(productsMeta)}
@@ -134,6 +173,25 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   actions: bindActionCreators({loadProducts}, dispatch),
+});
+
+const stylesa = StyleSheet.create({
+  rowBack: {
+    alignItems: 'center',
+    backgroundColor: '#DDD',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  rowPlaceholder: {
+    flex: 0.6,
+  },
+  rowMenu: {
+    flex: 0.4,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
