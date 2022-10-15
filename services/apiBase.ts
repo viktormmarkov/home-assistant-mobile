0f6253f9264a15a87fe98d1f36d918835a6382e4
Victor Markov <victor.markov@urbanise.com>
2022-10-15 16:58:35 +0300

    temp

--- services/apiBase.ts
+++ services/apiBase.ts
@@ -2,7 +2,9 @@ import axios from 'axios';
 const localURL = 'http://localhost:3031/'; 
 const baseURL = 'http://ec2-13-53-171-61.eu-north-1.compute.amazonaws.com/'; 
 
+// const ngrok = ' https://d73eeb77c519.eu.ngrok.io'; 
+
 export default axios.create({
-    baseURL: localURL,
+    baseURL: baseURL,
     withCredentials: true
 });
